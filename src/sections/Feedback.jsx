67a958ba6b4a000a4e50eea97bd69dcc05c72381
PieroVillanueva <pierofabrizzio9@hotmail.com>
2023-10-27 16:53:30 -0500

    1.1 corregido error para deploy

--- src/sections/Feedback.jsx
+++ src/sections/Feedback.jsx
@@ -26,9 +26,9 @@ const Feedback = () => {
               Founder | Metaversus
             </p>
             <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
-              “With the development of today's technology, metaverse is very
-              useful for today's work, or can be called web 3.0. by using
-              metaverse you can use it as anything”
+              “With the development of today`&apos;`s technology, metaverse is
+              very useful for today`&apos;`s work, or can be called web 3.0. by
+              using metaverse you can use it as anything”
             </p>
           </div>
         </motion.div>
